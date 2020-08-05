d837dfd45d1c75b6cc9d344a56cc846511321f24
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-05 11:17:45 +0300

    Initial Commit

---
+++ HowToCreateAPickerInSwiftUI/HowToCreateAPickerInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateAPickerInSwiftUIApp.swift
+//  HowToCreateAPickerInSwiftUI
+//
+//  Created by ramil on 05.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateAPickerInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
